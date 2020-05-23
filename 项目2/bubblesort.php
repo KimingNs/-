cfb453f9568117ef8fca09720554823731192922
KimingNs <648063762@qq.com>
2020-05-23 14:57:13 +0800

    冒泡排序

--- 项目2/bubblesort.php
+++ 项目2/bubblesort.php
@@ -2,5 +2,19 @@
 
 function bubbleSort($arr)
 {
+    for ($i = 0, $j = count($arr); $i < $j; $i++) {
+        for ($k = $j - 1; $k > $i; $k++) {
+            if ($arr[$k] < $arr[$k - 1]) {
+                $t = $arr[$k];
+                $arr[$k] = $arr[$k - 1];
+                $arr[$k - 1] = $t;
+            }
+        }
+    }
 
+    return $arr;
 }
+
+$arr = array(3, 2, 2, 4, 1, 5, 6, 9, 8, 7, 10);
+echo '<pre>';
+print_r(bubbleSort($arr));
