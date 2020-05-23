5ea56a303ff4af339dd99b1c199f0a89d87aae15
KimingNs <648063762@qq.com>
2020-05-23 15:25:45 +0800

    冒泡排序修改

--- 项目2/bubblesort.php
+++ 项目2/bubblesort.php
@@ -1,20 +1,24 @@
 <?php
 
+
 function bubbleSort($arr)
 {
-    for ($i = 0, $j = count($arr); $i < $j; $i++) {
-        for ($k = $j - 1; $k > $i; $k++) {
-            if ($arr[$k] < $arr[$k - 1]) {
-                $t = $arr[$k];
-                $arr[$k] = $arr[$k - 1];
-                $arr[$k - 1] = $t;
+// 第一层可以理解为从数组中键为0开始循环到最后一个
+    for ($i = 0; $i < count($arr); $i++) {
+        // 第二层为从$i+1的地方循环到数组最后
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            // 比较数组中两个相邻值的大小
+            if ($arr[$i] > $arr[$j]) {
+                $t = $arr[$i]; // 这里临时变量，存贮$i的值
+                $arr[$i] = $arr[$j]; // 第一次更换位置
+                $arr[$j] = $t; // 完成位置互换
             }
         }
     }
-
     return $arr;
 }
 
-$arr = array(3, 2, 2, 4, 1, 5, 6, 9, 8, 7, 10);
+// 定义一个随机的数组
+$arr = array(23, 15, 43, 25, 54, 2, 6, 82, 11, 5, 21, 32, 65);
 echo '<pre>';
 print_r(bubbleSort($arr));
