1e9b9bde5e1e21748e060a3d40d5446b84991f12
KimingNs <648063762@qq.com>
2020-05-23 14:29:49 +0800

    打印相邻月份头与尾的信息

--- 项目2/day.php
+++ 项目2/day.php
@@ -3,15 +3,42 @@
 //上个月第一天
 function getLastMonthBegin()
 {
-    echo date('Y-m-01', strtotime('-1 month')) . '<br/>';
+    $day = date('Y-m-01', strtotime('-1 month'));
+    $weekday = date("l", strtotime($day));
+
+    echo $day . $weekday . '<br/>';
 
 }
 
 //上个月最后一天
 function getLastMonthFinal()
 {
-    echo date('Y-m-t', strtotime('-1 month')) . '<br/>';
+    $day = date('Y-m-t', strtotime('-1 month'));
+    $weekday = date("l", strtotime($day));
+
+    echo $day . $weekday . '<br/>';
+
+}
+
+function getNextMonthBegin()
+{
+    $day = date('Y-m-01', strtotime('1 month'));
+    $weekday = date("l", strtotime($day));
+
+    echo $day . $weekday . '<br/>';
+
+}
+
+function getNextMonthFinal()
+{
+    $day = date('Y-m-t', strtotime('1 month'));
+    $weekday = date("l", strtotime($day));
+
+    echo $day . $weekday . '<br/>';
+
 }
 
 getLastMonthBegin();
 getLastMonthFinal();
+getNextMonthBegin();
+getNextMonthFinal();
