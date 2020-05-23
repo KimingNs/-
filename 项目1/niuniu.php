759ebbf05634f3e9cd318dbb4f2e3bac0ec42c81
KimingNs <648063762@qq.com>
2020-05-23 21:28:43 +0800

    加入solo玩法

--- 项目1/niuniu.php
+++ 项目1/niuniu.php
@@ -30,18 +30,74 @@ function niuniu_card()
         $tmp = array_values($tmp);
     }
 
+    //多人模式
+//    $card['user1']['cards'] = array_slice($cards, 0, 5, true);
+//    ksort($card['user1']['cards']);
+//    echo '张天明: <br/> ' . implode('  ', $card['user1']['cards']) . '<br /><br/>';
+//
+//    $card['user2']['cards'] = array_slice($cards, 6, 5, true);
+//    ksort($card['user2']['cards']);
+//    echo '张子恒:  <br/>' . implode('  ', $card['user2']['cards']) . '<br /><br/>';
+//
+//    $card['user3']['cards'] = array_slice($cards, 11, 5, true);
+//    ksort($card['user3']['cards']);
+//    echo '王琪:  <br/>' . implode('  ', $card['user3']['cards']) . '<br /><br/>';
 
-    $card['user1']['cards'] = array_slice($cards, 0, 5, true);
-    ksort($card['user1']['cards']);
-    echo '张天明: <br/> ' . implode('  ', $card['user1']['cards']) . '<br /><br/>';
+    //solo模式
+    for ($i = 1; $i <= 3; $i++) {
+        if ($i == 1) {
+            $card['user1']['cards'] = array_slice($cards, 0, 3, true);
+            $a1 = $card['user1']['cards'];
+            ksort($card['user1']['cards']);
+            echo '张天明: <br/> ' . implode('  ', $card['user1']['cards']) . '<br /><br/>';
 
-    $card['user2']['cards'] = array_slice($cards, 6, 5, true);
-    ksort($card['user2']['cards']);
-    echo '张子恒:  <br/>' . implode('  ', $card['user2']['cards']) . '<br /><br/>';
+            $card['user2']['cards'] = array_slice($cards, 3, 3, true);
+            $b1 = $card['user2']['cards'];
+            ksort($card['user2']['cards']);
+            echo '张子恒:  <br/>' . implode('  ', $card['user2']['cards']) . '<br /><br/>';
 
-    $card['user3']['cards'] = array_slice($cards, 11, 5, true);
-    ksort($card['user3']['cards']);
-    echo '王琪:  <br/>' . implode('  ', $card['user3']['cards']) . '<br /><br/>';
+            echo '第一轮游戏规则：<br/>' . '1.第一轮叫价以牌小优先。牌大小先看牛，无牛则看单牌大小及花色，有牛》无牛，若都有牛则比单牌大小及花色<br/>' .
+                '2.第一轮最多只能叫2倍底钱，先叫者为牌型较小者，后叫者可跟可不跟，跟则下一轮发牌，不跟则结束本局。<br/>';
+
+            echo '<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>';
+        } else if ($i == 2) {
+            $card['user1']['cards'] = array_slice($cards, 6, 1, true);
+            $a2 = $card['user1']['cards'];
+            ksort($card['user1']['cards']);
+            echo '张天明: <br/> ' . implode('  ', $card['user1']['cards']) . '<br /><br/>';
+
+            $card['user2']['cards'] = array_slice($cards, 7, 1, true);
+            $b2 = $card['user2']['cards'];
+            ksort($card['user2']['cards']);
+            echo '张子恒:  <br/>' . implode('  ', $card['user2']['cards']) . '<br /><br/>';
+
+            echo '第二轮游戏规则：<br/>' . '1.第二轮最多可以叫3倍底钱，先叫者为第一轮后叫者.<br/>' . '2.后叫者可跟可不跟，跟则下一轮发牌，不跟则结束本局。<br/>' .
+                '3.假如先叫者叫的倍数底钱较小，后叫者可反叫比先叫者倍数高的底钱，那么先叫者须补剩余倍数的底钱，或者不跟，跟则下一轮发牌，不跟则结束本局<br/>';
+            echo '<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>';
+        } else {
+
+            $card['user1']['cards'] = array_slice($cards, 8, 1, true);
+            $a3 = $card['user1']['cards'];
+            ksort($card['user1']['cards']);
+            echo '张天明: <br/> ' . implode('  ', $card['user1']['cards']) . '<br /><br/>';
+
+            $card['user2']['cards'] = array_slice($cards, 9, 1, true);
+            $b3 = $card['user2']['cards'];
+            ksort($card['user2']['cards']);
+            echo '张子恒:  <br/>' . implode('  ', $card['user2']['cards']) . '<br /><br/>';
+
+            echo '<br/>';
+
+        }
+
+    }
+
+    $a = array_merge($a1, $a2, $a3);
+    $b = array_merge($b1, $b2, $b3);
+
+    echo '张天明: <br/>最终牌为： ' . implode('  ', $a) . '<br /><br/>';
+    echo '张子恒: <br/>最终牌为： ' . implode('  ', $b) . '<br /><br/>';
+    echo '最终轮游戏规则：<br/>' . '1.牛大者获胜，赢得全部底钱<br/>' . '2.若有倍数牛则输家须给该倍数的底钱<br/><br/>';
 }
 
 /*
